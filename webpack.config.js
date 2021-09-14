55ef3b714e32ffece216da14bc8897dc87a2212f
Veselov <ddanil82@bk.ru>
2021-09-14 19:15:45 +0300

    my start

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const fs = require('fs')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//
+
+const PATHS = {
+    src: path.join(__dirname, '/src'),
+    dist: path.join(__dirname, '/dist'),
+    assets: 'assets/'
+}
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+        // analytics: './src/analytics.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js', // name динамически указывает на main и assets
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/pug/pages/index.pug'
+            // template: './src/index.html'
+            // template: './src/Pug/dev/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        ...PAGES.map(page => new HTMLWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/, '.html')}`
+        }))
+    ],
+    devServer: {
+        open: true,
+        port: 4200
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            }
+        ]
+    }
+}
